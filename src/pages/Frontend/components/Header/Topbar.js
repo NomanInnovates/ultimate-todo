f4f1566f124b75cc6b37982ab8099088909564fb
Nomi0125 <nomansandhu0125@gmail.com>
2022-08-03 00:00:25 +0500

    footer

--- src/pages/Frontend/components/Header/Topbar.js
+++ src/pages/Frontend/components/Header/Topbar.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
 function Topbar() {
-  return (
-    <div>Topbar</div>
-  )
+
+    const [currentTime, setCurrentTime] = useState("")
+    useEffect(() => {
+       setInterval(()=> {
+
+           setCurrentTime(dayjs().format('dddd'))
+       })
+    }, [])
+
+
+    return (
+        <div className="bg-primary py-1"> <div className='container'>
+            <div className='row'>
+                <div className='col '>
+
+                    <p className='mb-0 text-center text-white '>
+                        {currentTime}</p>
+                </div>
+            </div>
+
+        </div></div>
+    )
 }
 
 export default Topbar
