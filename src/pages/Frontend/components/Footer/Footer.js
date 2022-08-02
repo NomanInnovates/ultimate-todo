05ab1a677bf77aa94ddd42773663594890599666
Nomi0125 <nomansandhu0125@gmail.com>
2022-08-02 23:53:02 +0500

    footer

--- src/pages/Frontend/components/Footer/Footer.js
+++ src/pages/Frontend/components/Footer/Footer.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function Footer() {
     let year = new Date().getFullYear()
     return (
-        <footer className="bg-dark py-2">
+        <footer className="bg-dark   py-2">
             <div className='container'>
                 <div className='row'>
-                    <div className='col'></div>
-                    <p className='mb-0 text-center text-white '></p>
-                    &copy; {year} all right reserved.
+                    <div className='col '>
+
+                    <p className='mb-0 text-center text-white '>
+                    &copy; {year} all right reserved.</p>
+                    </div>
                 </div>
 
             </div>
--- src/pages/Frontend/index.js
+++ src/pages/Frontend/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import Home from './Home'
 
 
 function index() {
   return (
     <>
-    <Routes>
+      <Header />
+      <main>
+
+      <Routes>
         <Route path="/">
-            <Route index element={<Home/>} />
+          <Route index element={<Home />} />
         </Route>
-    </Routes>
+      </Routes>
+      </main>
+      <Footer />
     </>
   )
 }
